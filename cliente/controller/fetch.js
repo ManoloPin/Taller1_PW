0ca87f60f6997b0e10654fbaedb3214b0426557b
ManoloPin <jpinillaca1@uniminuto.edu.co>
2020-09-09 23:02:49 -0500

    funciones nuevas

--- cliente/controller/fetch.js
+++ cliente/controller/fetch.js
@@ -1,5 +1,9 @@
 //Listeners------------------------------------------------------
 document.getElementById("enviar").addEventListener("click", function(){exe("enviar");});
+
+document.getElementById("modificar").addEventListener("click", function(){exe("modificar");});
+document.getElementById("eliminar").addEventListener("click", function(){exe("eliminar");});
+
 document.getElementById("cargar").addEventListener("click", function(){exe("cargar");});
 //Fin Listeners--------------------------------------------------
 
@@ -13,14 +17,23 @@ function exe(){
       data.append('enviar', document.getElementById('enviar').value);
    }
    else if(arguments[0]=="cargar"){
-      alert("Cargando");
+      alert("Cargando"); 
       data.append('cargar', document.getElementById('cargar').value);
+   }
+   else if(arguments[0] == "modificar"){
+      alert("modificando");
+      data.append('modificar', document.getElementById('modificar').value);
+   }
+   else if (arguments[0] == "eliminar"){
+      alert("eliminando");
+      data.append('eliminar', document.getElementById('eliminar'))
    }
    
    data.append('id', document.getElementById('id').value);
    data.append('name', document.getElementById('name').value);
    data.append('dir', document.getElementById('dir').value);
-
+   data.append('idL', document.getElementById('idL').value);
+   
    fetch(
       'http://localhost/ej1/servidor/controller/cIndex.php',
 
--- servidor/controller/cIndex.php
+++ servidor/controller/cIndex.php
@@ -22,7 +22,29 @@ else if(isset($_POST['cargar'])){
   //--1. Crear un objeto ClienteDAO
   $clienteDAO=new ClienteDAO();
   //--2. Crear un objeto cliente usando ClienteDAO
-  $cliente=$clienteDAO->cargarCliente($_POST['id']);
+  $cliente=$clienteDAO->cargarCliente($_POST['idL']);
   //Enviar response
   echo $cliente->toString();
+}
+else if(isset($_POST['modificar'])){
+  $r=null;
+  //Guardar datos en la base de datos
+  //--1. Crear cliente
+  $cliente=new Cliente($_POST['id'], $_POST['name'], $_POST['dir']);
+  //--2. Crear un objeto clienteDAO
+  $clienteDAO=new ClienteDAO();
+  //--3. Guardar el cliente usando el ClienteDAO
+  $r=$clienteDAO->actualizarCliente($cliente);
+  
+  //Enviar response
+  echo $r[0]; 
+}
+else if(isset($_POST['eliminar'])){
+  //Cargar datos de un cliente usando la cédula
+  //--1. Crear un objeto ClienteDAO
+  $clienteDAO=new ClienteDAO();
+  //--2. Crear un objeto cliente usando ClienteDAO
+  $cliente=$clienteDAO->eliminarCliente($_POST['id']);
+  //Enviar response
+ // echo $r[0];
 }
